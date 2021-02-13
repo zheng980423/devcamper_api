7a22297bd7bdec6e989b558adc6b55b1a8a6f8a1
zwf <zwf19980423@gmail.com>
2021-02-14 01:50:59 +0800

    morgan and middleware set up

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,7 +2,9 @@
 //@route GET /api/v1/bootcamps
 //@access public
 exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+  res
+    .status(200)
+    .json({ success: true, msg: 'Show all bootcamps', hello: req.hello });
 };
 //@desc Get single bootcamp
 //@route GET /api/v1/bootcamps/:id
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+// const logger = require('./middleware/loggerMiddleware');
 //route files
 const bootcamps = require('./routes/bootcampRoutes');
 
@@ -7,6 +9,12 @@ const bootcamps = require('./routes/bootcampRoutes');
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+//Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+// app.use(logger);
 
 //Mount router
 app.use('/api/v1/bootcamps', bootcamps);
