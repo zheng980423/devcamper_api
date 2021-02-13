c3621b719794b967f5001c12600c769e0bd9252b
zwf <zwf19980423@gmail.com>
2021-02-14 01:08:44 +0800

    create express routes and response

--- server.js
+++ server.js
@@ -5,10 +5,26 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+});
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `get bootcamps ${req.params.id}` });
+});
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `updated bootcamps ${req.params.id}` });
+});
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `delete bootcamps ${req.params.id}` });
+});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(
-    `server running in ${process.env.NODE_ENV} listening on port ${PORT}!`
-  );
+  console.log(`Server running in ${process.env.NODE_ENV} MODE on port ${PORT}`);
 });
