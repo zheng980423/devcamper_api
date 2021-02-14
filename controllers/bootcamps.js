6b7b475a2d7ebf7220ece0dc496bdc2f58b7c846
zwf <zwf19980423@gmail.com>
2021-02-14 21:14:19 +0800

    bootcampModel

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const Bootcamp = require('../models/bootcampModel');
 //@desc Get all bootcamps
 //@route GET /api/v1/bootcamps
 //@access public
@@ -19,6 +20,7 @@ exports.getBootcamp = (req, res, next) => {
 //@route POST /api/v1/bootcamps
 //@access Private
 exports.createBootcamp = (req, res, next) => {
+  console.log(req.body);
   res.status(200).json({ success: true, msg: `Create new bootcamp` });
 };
 
