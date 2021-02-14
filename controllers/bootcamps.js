fb87c914592f611df0edf04edf22b63cf63afcc0
zwf <zwf19980423@gmail.com>
2021-02-14 21:28:52 +0800

    create bootcamp to mongodb

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -19,9 +19,18 @@ exports.getBootcamp = (req, res, next) => {
 //@desc Create new bootcamp
 //@route POST /api/v1/bootcamps
 //@access Private
-exports.createBootcamp = (req, res, next) => {
-  console.log(req.body);
-  res.status(200).json({ success: true, msg: `Create new bootcamp` });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+    res.status(201).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+    });
+  }
 };
 
 //@desc Update  bootcamp
--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const bootcamps = require('./routes/bootcampRoutes');
 
 const app = express();
 
+//Body parser
+app.use(express.json());
 //Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
