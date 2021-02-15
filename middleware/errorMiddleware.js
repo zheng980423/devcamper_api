538b898b88fa58b4c06a9f6efb91a0cdc4cc84b4
zwf <zwf19980423@gmail.com>
2021-02-15 12:57:07 +0800

    mongoose errorhandling 2

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -11,12 +11,23 @@ const errorHandler = (err, req, res, next) => {
   };
   error.message = err.message;
 
-  //mongoose bad objectId
+  //log error for dev
+  console.log(err);
+  //mongoose bad objectId id的长度不同
   if (err.name === 'CastError') {
     const message = `Resource not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
   }
-
+  //mongoose duplicate key 重复创建bootcamp
+  if (err.code === 11000) {
+    const message = 'duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+  //mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map(val => val.message);
+    error = new ErrorResponse(message, 400);
+  }
   res.status(error.statusCode || 500).json({
     succcess: false,
     error: error.message || 'Server Error',
