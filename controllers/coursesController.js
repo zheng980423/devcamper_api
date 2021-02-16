75629110d6c51fe7436b2f6d950144af853bf7fb
zwf <zwf19980423@gmail.com>
2021-02-17 00:13:23 +0800

    update course

--- controllers/coursesController.js
+++ controllers/coursesController.js
@@ -74,3 +74,23 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
     data: course,
   });
 });
+// @desc      update course
+// @route     PUT /api/v1/courses/:id
+// @access    Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with id of ${req.params.id}`, 404)
+    );
+  }
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
--- routes/coursesRoutes.js
+++ routes/coursesRoutes.js
@@ -3,9 +3,10 @@ const {
   getCourses,
   getCourse,
   addCourse,
+  updateCourse,
 } = require('../controllers/coursesController');
 const router = express.Router({ mergeParams: true });
 
 router.route('/').get(getCourses).post(addCourse);
-router.route('/:id').get(getCourse);
+router.route('/:id').get(getCourse).put(updateCourse);
 module.exports = router;
