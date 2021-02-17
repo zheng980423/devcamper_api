4b6eec2252260d0cf7cc7b9ced2e83b6032bf3ad
zwf <zwf19980423@gmail.com>
2021-02-17 17:38:03 +0800

    encrypt password

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,5 +5,16 @@ const User = require('../models/userModel');
 //@route GET /api/v1/auth/register
 //@access public
 exports.register = asyncHandler(async (req, res, next) => {
-  res.status(200).json({ success: true });
+  const { name, email, password, role } = req.body;
+
+  //create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
+  res.status(200).json({
+    success: true,
+  });
 });
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -34,4 +35,12 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
+//encrypt password using becrypt
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 module.exports = mongoose.model('User', UserSchema);
