31e6b0f4ad2766345998f15bf4ed7c6f6ee653e0
zwf <zwf19980423@gmail.com>
2021-02-18 12:18:02 +0800

    update user details

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,8 @@ const {
   getMe,
   forgotPassword,
   resetPassword,
+  updateDetails,
+  updatePassword,
 } = require('../controllers/authController');
 
 const router = express.Router();
@@ -13,6 +15,8 @@ const { protect } = require('../middleware/authMiddleware');
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 module.exports = router;
